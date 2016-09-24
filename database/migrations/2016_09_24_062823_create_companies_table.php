e7cc1475ac509a1b210d9e64725652f44d16b67a
Egor Talantsev <i.spyric@gmail.com>
2016-09-24 16:52:27 +0500

    add migrations

---
+++ database/migrations/2016_09_24_062823_create_companies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('avatar_url')->nullable();
+
+            $table->string('inn')->nullable();
+            $table->string('kpp')->nullable();
+            $table->string('ogrn')->nullable();
+            $table->string('bin')->nullable();
+            $table->string('settlement_account')->nullable();
+            $table->string('bank_account')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ database/migrations/2016_09_24_063814_create_flats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number');
+            $table->integer('men_count');
+            $table->integer('account_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flats');
+    }
+}
