4c4b6ec75f0cee0cae5230704aff8e657d74b4da
Андрй Тарасьев <andrew4800@mail.ru>
2016-09-25 14:35:38 +0500

    voting routes

--- app/Voting.php
+++ app/Voting.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Voting extends Model
 {
     protected $fillable = [
-        'name', 'house_id', 'closet_at'
+        'name', 'house_id', 'closed_at'
     ];
 
 
