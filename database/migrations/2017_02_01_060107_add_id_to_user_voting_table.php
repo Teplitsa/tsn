bd39c39b74d78bac0d58270b815b1014486efc1d
Egor Talantsev <spyric@ananas-web.ru>
2017-02-09 18:11:39 +0500

    fix

---
+++ database/migrations/2017_02_01_060107_add_id_to_user_voting_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIdToUserVotingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_voting', function (Blueprint $table) {
+            $table->dropPrimary();
+        });
+        Schema::table('user_voting', function (Blueprint $table) {
+            $table->increments('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table('user_voting', function (Blueprint $table) {
+            $table->dropColumn('id');
+        });
+        Schema::table('user_voting', function (Blueprint $table) {
+            $table->primary(['user_id', 'voting_id']);
+        });
+    }
+}
