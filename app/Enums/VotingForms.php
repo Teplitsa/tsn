2006d80ac3a0beb3ec7443652820b4d845faccc2
Ксения Ниссхен <kseniya_nisshen@mail.ru>
2017-05-16 11:37:13 +0500

    fix

--- app/Enums/VotingForms.php
+++ app/Enums/VotingForms.php
@@ -10,17 +10,15 @@ class VotingForms
     use TEnum;
 
 
-    const FULLTIME = 'очное',
+    const
         PARTTIME = 'очно-заочное',
         INABSENTIA = 'заочное';
 
     public static $values = [
-        self::FULLTIME   => 'очное',
         self::PARTTIME   => 'очно-заочное',
         self::INABSENTIA => 'заочное',
     ];
     public static $faker = [
-        self::FULLTIME   => 'очное',
         self::PARTTIME   => 'очно-заочное',
         self::INABSENTIA => 'заочное',
     ] ;
